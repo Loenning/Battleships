8cb83fcd5381c4d1df46aad73cbd50d12ff361a1
Loenning <Loenning1995@gmail.com>
2024-12-26 02:32:04 +0100

    Push for show

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,23 +1,90 @@
+interface Cell {
+    element: HTMLDivElement;
+    state: string;
+}
 
-//------ Creating the game board
+function createGrid(x, y, container) {
+    const grid = [];
 
-var createGrid=function(x,y, container){
-    var arrY = new Array(),
-        arrX;
-    for(var iy=0; iy<y; iy++){
-        arrX = new Array();
-        for(var ix=0; ix<x; ix++){
-            arrX[ix]='<div class="cell">&nbsp;</div>';
+    for (let row = 0; row < y; row++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        const rowCells = [];
+
+        for (let col = 0; col < x; col++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            rowDiv.appendChild(cell);
+            rowCells.push({ element: cell, state: 'water' });
         }
-        arrY[iy]='<div class="row">'+arrX.join("\r\n")+'</div>';
+
+        grid.push(rowCells);
+        container.appendChild(rowDiv);
     }
-    container.append(arrY.join("\r\n"));
-};
-// call function
-createGrid(10,10, $(".tablePlayer1"));
-createGrid(10,10, $(".tablePlayer2"));
 
+    return grid;
+}
 
+// Get the container elements
+const tablePlayer1 = document.getElementById('tablePlayer1');
+const tablePlayer2 = document.getElementById('tablePlayer2');
 
+// Create grids and store the grid data
+const player1Grid = createGrid(10, 10, tablePlayer1);
+const player2Grid = createGrid(10, 10, tablePlayer2);
 
+const water = 'water';
+const ship = 'ship';
+
+// Function to place a ship on the grid
+function placeShip(grid, length, orientation) {
+    let row, col;
+    let validPlacement = false;
+
+    while (!validPlacement) {
+        validPlacement = true;
+
+        // Get random starting position
+        row = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 10);
+
+        // Determine placement based on orientation
+        if (orientation === 'horizontal') {
+            if (col + length - 1 > 9) {
+                validPlacement = false;
+                continue;
+            }
+            for (let i = 0; i < length; i++) {
+                if (grid[row][col + i].state !== 'water') {
+                    validPlacement = false;
+                    break;
+                }
+            }
+        } else if (orientation === 'vertical') {
+            if (row + length - 1 > 9) {
+                validPlacement = false;
+                continue;
+            }
+            for (let i = 0; i < length; i++) {
+                if (grid[row + i][col].state !== 'water') {
+                    validPlacement = false;
+                    break;
+                }
+            }
+        }
+
+        // Place the ship
+        if (validPlacement) {
+            if (orientation === 'horizontal') {
+                for (let i = 0; i < length; i++) {
+                    grid[row][col + i].state = ship;
+                }
+            } else if (orientation === 'vertical') {
+                for (let i = 0; i < length; i++) {
+                    grid[row + i][col].state = ship;
+                }
+            }
+        }
+    }
+}
 
